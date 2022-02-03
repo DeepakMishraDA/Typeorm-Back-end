f91d916d6cfbac11a4852d7911b297431495d904
DeepakMishraDA <deepakvinodmishra@gmail.com>
2022-02-03 08:01:16 +0100

    commit

--- src/entity-schemas/Clients.ts
+++ src/entity-schemas/Clients.ts
@@ -1,21 +1,58 @@
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  BaseEntity,
+  CreateDateColumn,
+} from "typeorm";
 
 @Entity()
 export class Client extends BaseEntity {
-  @PrimaryGeneratedColumn()
+  @PrimaryColumn()
   id: number;
 
   @Column()
-  name: string;
+  firstname: string;
 
   @Column()
-  email: string;
-
-  @Column()
-  age: string;
+  lastname: string;
 
   @Column({
     unique: true,
   })
-  card_number: number;
+  email: string;
+
+  @Column({
+    unique: true,
+    length: 10,
+  })
+  card_number: string;
+
+  @Column({
+    type: "numeric",
+  })
+  amount: number;
+
+  @Column({
+    default: "is_active",
+  })
+  status: string;
+
+  @Column({
+    type: "simple-json",
+    nullable: true,
+  })
+  additional_info: {
+    age: number;
+    contact: number;
+  };
+
+  @Column({
+    type: "simple-array",
+    default: [],
+  })
+  address: string[];
+
+  @CreateDateColumn()
+  created_on: Date;
 }
